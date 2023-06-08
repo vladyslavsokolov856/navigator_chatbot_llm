76648279f6c1b3357f3ff9759285e84f3f5c6255
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2023-06-08 17:56:52 -0400

    added for docker deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     MONGODB_URI: process.env.MONGODB_URI,
